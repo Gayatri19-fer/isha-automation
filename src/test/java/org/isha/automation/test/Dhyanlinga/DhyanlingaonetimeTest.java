755f418f3ce6bc80d54f816fa748d17aa80f8914
Gayatri <gayatrib@fermion.in>
2026-03-27 10:56:39 +0530

    dhyanlinga main folder changes

---
+++ src/test/java/org/isha/automation/test/Dhyanlinga/DhyanlingaonetimeTest.java
@@ -0,0 +1,44 @@
+package org.isha.automation.test.Dhyanlinga;
+
+import org.isha.automation.basetest.BaseTest;
+import org.isha.automation.basetest.Retry;
+import org.ishafoundation.pages.Sadhguru.Dhyanlinga.DhyanlingaCanclePage;
+import org.ishafoundation.pages.Sadhguru.Dhyanlinga.DhyanlingaDonateapage;
+import org.ishafoundation.pages.Sadhguru.Dhyanlinga.DhyanlingapaymentPage;
+import org.ishafoundation.pages.Sadhguru.Dhyanlinga.DhyanlingapersonaldetailsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Page;
+
+public class DhyanlingaonetimeTest extends BaseTest {
+	
+	@Test(groups= {"sanity"},retryAnalyzer = Retry.class)
+	public void shivangaOntimeflow() throws InterruptedException {
+		Page page = getPage(); // always get page from BaseTest
+		page.navigate("https://isha.sadhguru.org/en/contribute/dhyanalinga/donate");
+		DhyanlingaDonateapage DD = new DhyanlingaDonateapage(page);
+		DD.SelectAmount();
+		DD.Continue();
+		DhyanlingapersonaldetailsPage DP = new DhyanlingapersonaldetailsPage(page);
+		DP.EnterFirstname();
+		DP.EnterLasttname();
+		DP.EnterEmail();
+		DP.EnterPhonenumber();
+		DP.Selectcitizenship();
+		DP.Select80GTax();
+		DP.Selectcountry();
+		DP.Selectstate();
+		DP.Entertcity();
+		DP.EnterAddress();
+		DP.EnterPincode();
+		DP.EnterPan();
+		DP.Submit();
+		DhyanlingapaymentPage DPP = new DhyanlingapaymentPage(page);
+		DPP.payucancle();
+		DhyanlingaCanclePage DC = new DhyanlingaCanclePage(page);
+		Assert.assertTrue(DC.isfailedPageOpen());	//  to validate fail page open or not
+		DC.canclemsg();
+		DC.getPageUrl();
+	}
+}
