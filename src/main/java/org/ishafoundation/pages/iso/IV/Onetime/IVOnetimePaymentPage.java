abb769329b74d97658aeecb781d317be9c496cc6
Gayatri <gayatrib@fermion.in>
2026-05-29 10:48:11 +0530

    IV payu changes

--- src/main/java/org/ishafoundation/pages/iso/IV/Onetime/IVOnetimePaymentPage.java
+++ src/main/java/org/ishafoundation/pages/iso/IV/Onetime/IVOnetimePaymentPage.java
@@ -126,4 +126,17 @@ public class IVOnetimePaymentPage {
 
 			    confirmBtn.click(new Locator.ClickOptions().setTimeout(10000));
 		}
+
+		public void payucancle() {
+			page.click("//i[@id='sidebar-backBtn']");
+			Locator button = page.locator("//button[normalize-space()='YES, CANCEL']");
+			button.waitFor();
+			button.click();
+			Locator selectradio = page.locator("(//label[contains(@class,'cstm-radio')])[1]");
+			selectradio.waitFor();
+			selectradio.click();
+			Locator submit = page.locator("//button[normalize-space()='Submit']");
+			submit.click();
+		}
+
 }
